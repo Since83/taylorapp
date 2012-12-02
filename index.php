c02d541d932e3077febc673639eab017aafc7985
Eric Hamby <Eric@EricHamby.Com>
2012-12-02 07:09:13 -0800

    Deleted the page to rename it index.php

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="generator" content="83Hosting.Com" />
+<title>Groceries.Com | CrowdSourcing Groceries And More</title>
+
+<!-- Bookmark Icon -->
+<link rel="shortcut icon" href="favicon.ico" />
+
+<!-- Stylesheet -->
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<style type="text/css">
+a {
+	color: #47AFD5;
+}
+</style>
+
+<!-- Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Merriweather:400,700,900' rel='stylesheet' type='text/css' />
+</head>
+
+<body class="home blog ">
+<div id="main">
+  <div id="header" class="clearfix">
+    <div id="logo"> <a href="index.php" title="radar"><img src="img/logo.png" alt="" /></a> </div>
+    
+    <!-- Full size menu -->
+    <div class="menu-container">
+      <ul class="nav">
+        <li class="current button orange"> 
+          <a href="#">Login</a>
+          </script></li>
+         
+          <li class="current button orange"> 
+          <a href="#">Signup</a>
+          </script></li>
+        <li>Please login or sign up to use this service</li>
+      </ul>
+    </div>
+    <!-- menu --> 
+  </div>
+  <!--end header -->
+  
+  <div id="page" class="clearfix">
+    <div id="white-wrap">
+      <div class="section clearfix"> 
+        
+        <!-- launch tab -->
+        <div id="launch" class="box">
+          <h2 class="launch-title">Welcome to HERE.Com, </h2>
+          <p>Thank you for choosing us. No matter if you just need a little extra cash, or your unable to make it to the store. Our service will help you. below is a simple chart on how the service works.</p>
+          
+          <img src="img/whatsgoingon.png" />
+        </div>
+        <!-- box --> 
+        
+      </div>
+      <!-- section --> 
+    </div>
+    <!-- white wrap --> 
+  </div>
+  <!-- page --> 
+</div>
+<!-- main -->
+
+<div id="footer">
+  <div id="icons"> 
+    <!-- social media icons --> 
+<a target="blank" href="#" title="twitter"><img src="img/icon-twitter.png" alt="twitter" /></a> 
+
+<a target="blank" href="#" title="google"><img src="img/icon-google.png" alt="google" /></a> 
+<a target="blank" href="#" title="rss feed"><img src="img/icon-rss.png" alt="rss" /></a> 
+
+<a target="blank" href="#" title="facebook"><img src="img/icon-facebook.png" alt="facebook" /></a> 
+
+<a target="blank" href="#" title="dribbble"><img src="img/icon-dribbble.png" alt="dribbble" /></a> 
+
+<a target="blank" href="#" title="vimeo"><img src="img/icon-vimeo.png" alt="vimeo" /></a> 
+
+<a target="blank" href="#" title="tumblr"><img src="img/icon-tumblr.png" alt="tumblr" /></a> 
+<a target="blank" href="#" title="youtube"><img src="img/icon-youtube.png" alt="youtube" /></a> 
+
+<a target="blank" href="#" title="pinterest"><img src="img/icon-pinterest.png" alt="pinterest" /></a> </div>
+  <!-- social media icons -->
+  
+  <div class="footer-text">
+    <p class="copy">Copyright 2012-2013 — <a href="http://www.83hosting.com" title="83Hosting">83Hosting.Com</a> @ <small>Las Vegas, NV.</small></p>
+  </div>
+  <!-- footer-text --> 
+</div>
+<!-- footer -->
+</body>
+</html>
